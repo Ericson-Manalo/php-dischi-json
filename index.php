6b267122dfd2cc5c35683d64c7d29e8dc7752726
Ericson-Manalo <ericmana98@gmail.com>
2023-07-05 15:59:57 +0200

    imported by cdn axios, vue and my css link

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--bootstrap-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!--axios-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"></script>
+    <!--vue-->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!--my link to my css-->
+    <link rel="stylesheet" href="styles/style.css">
     <title>PHP Dischi Json</title>
 </head>
 <body>
