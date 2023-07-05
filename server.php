a5eeae7a627a53cf5c7b28bb7f32498fbb22f63f
Ericson-Manalo <ericmana98@gmail.com>
2023-07-05 16:40:38 +0200

    crearted variable disk and used json_decode

--- server.php
+++ server.php
@@ -2,10 +2,11 @@
 
     $stringRawData = file_get_contents ('./database/dischi.json');
 
+    $disk = json_decode($stringRawData);
     //var_dump($stringRawData);
 
     header('Content-Type: application/json');
-    echo json_encode($stringRawData);
+    echo json_encode($disk);
 
 
 
