5c75bfdb3c3d42d06db3267134667a1163c92377
Ericson-Manalo <ericmana98@gmail.com>
2023-07-05 16:45:59 +0200

    called axios

--- script/script.js
+++ script/script.js
@@ -3,10 +3,28 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            apiUrl : './server.php'
         }
     },
     methods: {
-        
+        getDisks(){
+            axios.get(this.apiUrl, {
+                params: {
+                    }
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+                .finally(function () {
+                    // always executed
+                });  
+        }
     },
+
+    created(){
+        this.getDisks();
+    }
 }).mount('#app')
